1eaf1a61f515a97824b5d6b37a9904ee186a8be6
Jason Ruff <ruffjason@gmail.com>
2025-01-23 06:36:35 -0700

    first commit

---
+++ scripts.js
@@ -0,0 +1,17 @@
+async function fetchBitcoinPrice() {
+    const apiKey = coinGeckoAPI;
+    const baseUrl = 'https://api.coingecko.com/api/v3/';
+    const endpoint = 'simple/price?ids=bitcoin,dogecoin&vs_currencies=usd';
+    
+    try {
+        const response = await fetch(`${baseUrl}${endpoint}&x_cg_api_key=${apiKey}`);
+        const data = await response.json();
+        document.getElementById('bitcoin-price').textContent = `Bitcoin's Current price is $:${data.bitcoin.usd.toLocaleString()}`;
+        document.getElementById('doge-price').textContent = `Doge Coin's Current price is $:${data.dogecoin.usd.toLocaleString()}`;
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+// Call the function when page loads
+fetchBitcoinPrice();
